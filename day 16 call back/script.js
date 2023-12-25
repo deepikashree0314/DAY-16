6c55a6f9d3be1d16520472f082e8c8ffd69dcc6f
deepikashree0314 <150881207+deepikashree0314@users.noreply.github.com>
2023-12-25 21:50:40 +0530

    Add files via upload

---
+++ day 16 call back/script.js
@@ -0,0 +1,40 @@
+
+setTimeout(()=>{
+    document.querySelector('#four').innerHTML = "09"
+     setTimeout(()=>{
+        document.querySelector('#four').innerHTML = "08"
+         setTimeout(()=>{
+            document.querySelector('#four').innerHTML = "07"
+            setTimeout(()=>{
+                document.querySelector('#four').innerHTML = "06"
+                 setTimeout(()=>{
+                    document.querySelector('#four').innerHTML = "05"
+                     setTimeout(()=>{
+                        document.querySelector('#four').innerHTML = "04"
+                        setTimeout(()=>{
+                            document.querySelector('#four').innerHTML = "03"
+                            setTimeout(()=>{
+                                document.querySelector('#four').innerHTML = "02"
+                                 setTimeout(()=>{
+                                    document.querySelector('#four').innerHTML = "01"
+                                    setTimeout(()=>{
+                                        document.querySelector('#four').innerHTML = "00"
+                                         setTimeout(()=>{
+                                            document.body.style.backgroundColor="#fff"
+                                            document.body.innerHTML = `<div class="container">
+                                            <div class="countdown">
+                                            <img src="https://i.pinimg.com/originals/bd/e8/38/bde838d2b0e2b4959e9f0f5ebed29af7.gif" alt="HAPPY INDEPENDENCE DAY 🎇">
+
+                                            </div>
+                                        </div>`
+                                         })
+                                    },1000)
+                                 },1000)
+                            },1000)
+                        },1000)
+                     },1000)
+                 },1000)
+            },1000)
+         },1000)
+     },1000)
+},1000)
